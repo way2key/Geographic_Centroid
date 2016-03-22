7606c62b80e34cc134ad423e5c49b5386ca7a45e
way2key <olivier_dancona@hotmail.com>
2016-03-22 19:03:00 +0100

    Connexion to DataBase

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_HOST','localhost');
 define('DB_NAME','TM_MOB');
-define('DB_USERNAME','root');
-define('DB_PASSWORD','321654');
+define('DB_USERNAME','YOUR_DATABASE_USRNAME');
+define('DB_PASSWORD','PASSWORD');
 try{
 $db=new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8",DB_USERNAME,DB_PASSWORD);
 $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
